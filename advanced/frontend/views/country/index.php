7347c76c951f4eadfea4f4eabbb8addcbcc02f25
zhangsan <824206563@qq.com>
2017-03-11 17:11:09 +0800

    asdfa

--- advanced/frontend/views/country/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-use yii\widgets\LinkPager;
-?>
-<?php foreach($countrys as $key=>$value):?>
-<h1><?= $value['code'] ?>--------<?= $value['name'] ?></h1>
-<?php endforeach; ?>
-<div>
-<?= LinkPager::widget(['pagination' => $pagination]) ?>
-</div>
